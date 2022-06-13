262cd582fcccbe444cf80e8639b20026dec7ac91
Chester <chesteryj@163.com>
2022-06-14 00:03:49 +0800

    second commit

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -6,5 +6,6 @@ package com.study.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test");
     }
 }
