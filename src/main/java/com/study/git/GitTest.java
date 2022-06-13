077b220f3df4a437150913dcd6a3315d3700d9c4
Chester <chesteryj@163.com>
2022-06-13 23:46:35 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.study.git;
+
+/**
+ * Created by Chester on 2022/6/13.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
